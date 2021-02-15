762d16df9f196f557edd31171c52f48e4ae5da49
fchriste <70377452+fchriste@users.noreply.github.com>
2021-02-15 16:14:00 +0100

    FCH: update boxplots

--- PyCharm/imageprocessing/04_image_per_user.py
+++ PyCharm/imageprocessing/04_image_per_user.py
@@ -2,8 +2,6 @@ import geopandas as gpd
 import pandas as pd
 import json
 
-
-
 #define which data to use
 topic="rhb"
 
@@ -11,7 +9,7 @@ topic="rhb"
 max_contributions = 100
 
 #read in unesco shapefile
-flickr_data = gpd.read_file("data/02_points_in_polygon/flickr/%s_flickr.shp" %  topic)
+flickr_data = gpd.read_file("data/02_points_in_polygon/flickr/%s_flickr.shp" % topic)
 
 #define output path
 output_path= "data/03_image_per_user/%s.json" % topic
@@ -22,9 +20,8 @@ pd.set_option('display.max_rows', None)
 #print(flickr_data.download_u[0])
 print(flickr_data.head())
 
-
+#initiate dictionary for count
 dict_user={}
-
 
 for i in range(0,len(flickr_data.user_nsid)):
    #print(flickr_data.user_nsid[i])
@@ -34,13 +31,10 @@ for i in range(0,len(flickr_data.user_nsid)):
    else:
        dict_user[flickr_data.user_nsid[i]]=1
 
-
-
 print(dict_user)
 #save file as json for visualization with r
 with open(output_path, "w", encoding='utf-8') as outfile:
     json.dump(dict_user, outfile, ensure_ascii=False)
-
 
 #get users with more than 100 images
 frequent_user = dict((k, v) for k, v in dict_user.items() if v >= 100)
@@ -54,8 +48,6 @@ for i in frequent_user:
 # get subset of data without frequent users
 flickr_res = flickr_data[~flickr_data.user_nsid.isin(freq_list)]
 
-
-
 #iterate through every frequent user nto add random rows
 for user in frequent_user:
     #get data
@@ -65,9 +57,6 @@ for user in frequent_user:
     limited_flickr = user_data.sample(max_contributions)
     #append dataframe with random rows
     flickr_res=flickr_res.append(limited_flickr)
-
-
-
 
 print(len(flickr_res))
 
